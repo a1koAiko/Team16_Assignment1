e6e983331918989e66e8faa058c5ecacdce3fafe
a1koAiko <steve@coho.co>
2018-03-27 03:25:55 -0600

    eclipse rescue, 3-27-18

---
+++ test/esof322/pa1/Vector3DTest.java
@@ -0,0 +1,51 @@
+package esof322.pa1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Vector3DTest {
+
+	@Test
+	public void testScale() { // 
+		Vector3D testInstance = new Vector3D();
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd() { 
+		Vector3D testInstance = new Vector3D();
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSubtract() {
+		Vector3D testInstance = new Vector3D();
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testNegate() {
+		Vector3D testInstance = new Vector3D();
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDotProduct() {
+		Vector3D testInstance = new Vector3D();
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMagnitude() {
+		Vector3D testInstance = new Vector3D();
+		
+		fail("Not yet implemented");
+	}
+
+}
